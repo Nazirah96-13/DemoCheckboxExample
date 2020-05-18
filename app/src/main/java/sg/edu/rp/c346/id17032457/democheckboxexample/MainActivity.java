a21cfbc3eaacf261ae219d0a31b2c19e1008d764
NORNAZIRAH BINTE MOHAMED TALIB RP <17032457@myrp.edu.sg>
2020-05-18 13:07:50 +0800

    step added

--- app/src/main/java/sg/edu/rp/c346/id17032457/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id17032457/democheckboxexample/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //step 1
     CheckBox cbEnabled;
     Button btnCheck;
     TextView tvShow;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //step 2
         cbEnabled = findViewById(R.id.checkBoxEnable);
         btnCheck = findViewById(R.id.btnCheck);
         tvShow = findViewById(R.id.textView);
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         btnCheck.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
             Toast.makeText(MainActivity.this, "Button Click", Toast.LENGTH_LONG).show();
             if (cbEnabled.isChecked()){
                 tvShow.setText("The discount is given");
